cfc478b25afb56dd76dcd65e52827c7d84d10c8d
Nathan <100631453+igotthisaccountforuni@users.noreply.github.com>
2023-03-02 11:05:35 +1300

    update

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -1,50 +1,40 @@
-var x=300;
-var y=300;
-var a=100;
-var b=100;
-
-function draw_one_frame() {
-  background(255);
-  x+=2;
-  y+=2;
-	a-=2;
-	b-=2;
-	strokeWeight(1);
-  translate(width/2, height/2);
-  for(var i=0;i<15;i++){
-	  for(var k=0;k<20;k++){
-		stroke(255,255,255);
-    rotate(PI / 12.0);
-	  fill(235);
-  	line(a%100,b%100,x%300,y%300);
-	  ellipse((x+i*20)%width,(y+k*20)%height,i+4,i+4);
-		drawtriangle((a-i*20)%width,(b-k*20)%height,k/8);
-		rect(x%width, y%height, k+10, k+10);
-		fill(i*10);
-		ellipse((x-i*20)%width,(y-k*20)%height,i+3,i+3);
-		rotate(PI / 24.0);
-		fill(255-(i+k)*5);
-		drawtriangle((a+i*20)%width,(b+k*20)%height,k/8);
-		rect(a%width, b%height, k+5, k+5);
-		drawflower(k,x);
+function draw_one_frame(cur_frac) {
+	let sun_size = canvasHeight/8;
+  
+	noStroke();
+	fill(100, 100, 214);
+	rect(0, 0, width, height);
+  
+	fill(0, 200, 0);
+	rect(0, height/3, width, height*2/3);
+  
+	stroke(0);
+	line(width/2, 0, width/2, height/2);
+	line(0.30*width, 0, 0.10*width, height/2);
+	line(0.70*width, 0, 0.90*width, height/2);
+  
+	strokeWeight(10);
+	let grid_points = [
+	  0.0 * height,
+	  0.10 * height,
+	  0.20 * height,
+	  0.30 * height,
+	  0.40 * height,
+	  0.50 * height,
+	]
+  
+	if (debugView) {
+	  strokeWeight(1);
+	  stroke(255, 0, 0);
+	  for(let i=0; i<grid_points.length; i++) {
+		line(0, grid_points[i], width, grid_points[i]);
 	  }
+	}
+  
+	strokeWeight(2);
+	stroke(0);
+	for(let i=0; i<grid_points.length-1; i++) {
+	  let cur_grid_line = map(cur_frac, 0, 1, grid_points[i], grid_points[i+1])
+	  line(0, cur_grid_line, width, cur_grid_line);
+	}
   }
-
-}
-
-function drawtriangle(x,y,r){
-	triangle(x, y, x+7*r, y-13.75*r, x+14*r, y);
-}
-
-function drawflower(i,k){
-		if(i%2==1){
-			fill((k*0.4)%255);
-			stroke(k%255);
-			arc(0,0,150+i+150*sin(k*PI/24),150,0,PI / 40);
-		}
-		else{
-			fill(k%255);
-			stroke((k*0.4)%255);
-			arc(0,0,(100+100*cos(k*PI/24))%255,50,0,PI / 20);
-		}
-}
