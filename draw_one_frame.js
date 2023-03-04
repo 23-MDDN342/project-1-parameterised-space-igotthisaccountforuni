01d0e53366dfa81895edab51dbd1a561b0248ff9
Nathan <parrnathan8@gmail.com>
2023-03-05 10:11:33 +1300

    yep

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -1,30 +1,20 @@
 function draw_one_frame(cur_frac) {
+	// chopping board colour fill(235, 154, 90);
+	background(235, 154, 90)
 	noStroke();
-	fill(235, 154, 90);
-	rect(0, 0, width, height);
-  
-	fill(200, 119, 55);
-	rect(0, height/2, width, height/4);
+	fill(180);
+	rect(0, height * 0.4, width, 0.3*height )
 
-	fill(200);
-	rect(0, height*3/4, width, height/4);
-  
-	stroke(0);
-	line(0, height*3/4, width, height*3/4)
-	line(width/2, 0, width/2, height/2);
-	line(0.30*width, 0, 0, height/2);
-	line(0.70*width, 0, width, height/2);
-  
-	strokeWeight(10);
 	let grid_points = [
 	  0.0 * width,
-	  0.10 * width,
 	  0.20 * width,
-	  0.30 * width,
 	  0.40 * width,
-	  0.50 * width
+		0.60 * width,
+	  0.80 * width,
+	  1.00 * width,
+		1.20 * width
 	]
-  
+
 	if (debugView) {
 	  strokeWeight(1);
 	  stroke(255, 0, 0);
@@ -32,11 +22,16 @@ function draw_one_frame(cur_frac) {
 		line(0, grid_points[i], width, grid_points[i]);
 	  }
 	}
-  
-	strokeWeight(2);
-	stroke(0);
-	for(let i=0; i<grid_points.length-1; i++) {
-	  let cur_grid_line = map(cur_frac, 0, 1, grid_points[i], grid_points[i+1])
-	  line(0, cur_grid_line, width, cur_grid_line);
+
+	for(let i=0; i<grid_points.length-1; i++) { 																//conveyer belt + plates
+	  let curGridPos = map(cur_frac, 0, 1, grid_points[i], grid_points[i+1])
+		strokeWeight(0.2);
+		stroke(0);
+		line(curGridPos, 0.4 * height , curGridPos, 0.7 * height);
+		fill(240);
+		noStroke();
+		ellipse(curGridPos - 95 , 0.55 * height, 150, 150);
+		fill(235);
+		ellipse(curGridPos - 95 , 0.55 * height, 130, 130);
 	}
-  }
+}
