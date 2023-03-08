fbfba82cb66c7fd7c0e692a750fbed3a8bf1bf9d
Nathan <100631453+igotthisaccountforuni@users.noreply.github.com>
2023-03-09 11:15:49 +1300

    Update draw_one_frame.js

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -1,9 +1,9 @@
 function draw_one_frame(cur_frac) {
 	// chopping board colour fill(235, 154, 90);
-	background(245)
-	noStroke();
+	background(220)
+	stroke(0);
 	fill(180);
-	rect(0, height * 0.4, width, 0.3*height )
+	text('I have changed my idea', 10, 30)
 
 	// console.log(width);
 
@@ -13,9 +13,9 @@ function draw_one_frame(cur_frac) {
 	  0.40 * width,
 		0.60 * width,
 	  0.80 * width,
-	  1.00 * width,
-		1.20 * width
+	  1.00 * width
 	]
+
 
 	// if (debugView) {
 	//   strokeWeight(1);
@@ -25,70 +25,30 @@ function draw_one_frame(cur_frac) {
 	//   }
 	// }
 
-	for(let i=0; i<conveyerPoints.length-1; i++) { 																//conveyer belt + plates
-	  let curConveyerPos = map(cur_frac, 0, 1, conveyerPoints[i], conveyerPoints[i+1])
-		strokeWeight(0.2);
-		stroke(0);
-		line(curConveyerPos, 0.4 * height , curConveyerPos, 0.7 * height);
-		fill(240);
-		noStroke();
-		ellipse(curConveyerPos - width / 10.1 , 0.55 * height, width / 6.4, width / 6.4);
-		fill(235);
-		ellipse(curConveyerPos - width / 10.1 , 0.55 * height, width / 7.4, width / 7.4);
-	}
+		for (var i = 0; i < conveyerPoints.length; i++) {
+			fill(180);
+			rect(width* 0.45, 0, width * 0.1, height*4/9);
+			for(let i=0; i<conveyerPoints.length-2; i++) { 																//ginger conveyer
+				let curConveyerPos2 = map(cur_frac, 0, 1, conveyerPoints[i], conveyerPoints[i+1])
+				stroke(0);
+				line(width * 0.45, curConveyerPos2/4, width * 0.55 , curConveyerPos2/4);
+			}
+		}
 
-	for(let i=3; i<conveyerPoints.length-1; i++) { 																		//sushi
-	  let curFoodPos = map(cur_frac, 0, 1, conveyerPoints[i], conveyerPoints[i+1])
-		strokeWeight(height/120);
-		stroke(9, 82, 9);
-		fill(240);
-		ellipse(curFoodPos - width / 10.1, 0.55 * height, width / 19.2, width /19.2);
-	}
 
-	for(let i=2; i<conveyerPoints.length-1; i++) { 																		//wasabi
-	  let curFoodPos = map(cur_frac, 0, 1, conveyerPoints[i], conveyerPoints[i+1])
-		strokeWeight(5);
-		noStroke();
-		fill(105, 166, 7);
-		ellipse(curFoodPos - width/8, 0.625 * height, width / 38.4, width / 38.4);
-	}
-
-	for(let i=1; i<conveyerPoints.length-1; i++) { 																		//ginger
-	  let curFoodPos = map(cur_frac, 0, 1, conveyerPoints[i], conveyerPoints[i+1])
-		strokeWeight(5);
-		noStroke();
-		fill(232, 158, 118);
-		push();
-		rectMode(CENTER);
-		rect(curFoodPos - width/7.9, 0.475 * height, width / 38.4, width/48);
-		pop();
-	}
-
-		fill(200);
-		rect(width/21, 0, width / 18, height*4/9);
-		for(let i=0; i<conveyerPoints.length-2; i++) { 																//ginger conveyer
-		  let curConveyerPos2 = map(cur_frac, 0, 1, conveyerPoints[i], conveyerPoints[i+1])
-			strokeWeight(0.2);
+		rect(0, height * 0.4, width, 0.3*height )
+		for(let i=0; i<conveyerPoints.length-1; i++) { 																//conveyer belt + plates
+		  let curConveyerPos = map(cur_frac, 0, 1, conveyerPoints[i], conveyerPoints[i+1])
+			strokeWeight(0.5);
 			stroke(0);
-			line(width/19, curConveyerPos2/4, width/10, curConveyerPos2/4);
-			noStroke();
-			fill(232, 158, 118);
-			push();
-			rectMode(CENTER);
-			rect(width/13.5, curConveyerPos2/4, width / 38.4, width/48);
-			pop();
+			line(curConveyerPos, 0.4 * height , curConveyerPos, 0.7 * height);
 		}
-		fill(139, 163, 131);
-		beginShape();
-		vertex(width/4, height);
-		vertex(width/4, height/1.25);
-		vertex(width/3.75, height/1.55);
-		vertex(width/3.5, height/1.55);
-		vertex(width/3.3, height/1.25);
-		vertex(width/3.3, height);
-		endShape(CLOSE);
-		for(let i=2; i<conveyerPoints.length-2; i++) {
-			let curWasabiPos = map(cur_frac, 1, 0, conveyerPoints[i], conveyerPoints[i+1]);
-			ellipse(width/3.62, curWasabiPos/2 + height/3, width / 16, width/16);
-		}
+		// let swayPos
+		// if (cur_frac < 0.51) {
+		// 	swayPos = map(cur_frac, 0, 0.5, height/5, height*4/5 );
+		// }else{
+		// 	swayPos = map(cur_frac, 0.51, 1, height*4/5, height/5);
+		// }
+		// rectMode(CENTER);
+		// rect(width * 0.7, swayPos, 50, 50);
 }
