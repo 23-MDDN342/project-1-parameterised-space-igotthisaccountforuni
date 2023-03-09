5def3bee679a6edd824718ee4183c0a75e49ab5c
Nathan <100631453+igotthisaccountforuni@users.noreply.github.com>
2023-03-09 14:23:01 +1300

    Update draw_one_frame.js

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -7,7 +7,7 @@ function draw_one_frame(cur_frac) {
 
 	// console.log(width);
 
-	let conveyerPoints = [
+	let conveyerWidths = [
 	  0.0 * width,
 	  0.20 * width,
 	  0.40 * width,
@@ -16,29 +16,42 @@ function draw_one_frame(cur_frac) {
 	  1.00 * width
 	]
 
+	let conveyerHeights = [
+	  0.0 * height,
+	  0.20 * height,
+	  0.40 * height,
+		0.60 * height,
+	  0.80 * height,
+	  1.00 * height
+	]
+
 
 	// if (debugView) {
 	//   strokeWeight(1);
 	//   stroke(255, 0, 0);
-	//   for(let i=0; i<conveyerPoints.length; i++) {
-	// 	line(0, conveyerPoints[i], width, conveyerPoints[i]);
+	//   for(let i=0; i<conveyerWidths.length; i++) {
+	// 	line(0, conveyerWidths[i], width, conveyerWidths[i]);
 	//   }
 	// }
 
-		for (var i = 0; i < conveyerPoints.length; i++) {
-			fill(180);
-			rect(width* 0.45, 0, width * 0.1, height*4/9);
-			for(let i=0; i<conveyerPoints.length-2; i++) { 																//ginger conveyer
-				let curConveyerPos2 = map(cur_frac, 0, 1, conveyerPoints[i], conveyerPoints[i+1])
+		for (var i = 0; i < conveyerWidths.length; i++) {
+
+		}
+
+		fill(180);
+		for(let i=0; i<conveyerWidths.length; i++) { 																//ginger conveyer
+			rect(conveyerWidths[i] + width * 0.05, 0, width * 0.1, height);
+
+			for (var j = 0; j < conveyerWidths.length; j++) {
+				let curConveyerPos2 = map(cur_frac, 0, 1, conveyerWidths[i], conveyerWidths[i+1])
 				stroke(0);
-				line(width * 0.45, curConveyerPos2/4, width * 0.55 , curConveyerPos2/4);
+				line(conveyerWidths[j] + width * 0.05, curConveyerPos2/4, width * 0.55 , curConveyerPos2/4);
 			}
 		}
 
-
 		rect(0, height * 0.4, width, 0.3*height )
-		for(let i=0; i<conveyerPoints.length-1; i++) { 																//conveyer belt + plates
-		  let curConveyerPos = map(cur_frac, 0, 1, conveyerPoints[i], conveyerPoints[i+1])
+		for(let i=0; i<conveyerWidths.length-1; i++) { 																//conveyer belt + plates
+		  let curConveyerPos = map(cur_frac, 0, 1, conveyerWidths[i], conveyerWidths[i+1])
 			strokeWeight(0.5);
 			stroke(0);
 			line(curConveyerPos, 0.4 * height , curConveyerPos, 0.7 * height);
